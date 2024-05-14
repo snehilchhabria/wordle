0de887121cf70ceef69374128e347e3503aeaf5b
Snehil Chhabria <snehilchhabria143@gmail.com>
2024-05-14 23:45:56 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const state = {
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill('')),
+    currentRow: 0,
+    currentCol: 0,
+};
+
+function updateGrid() {
+    for(let i=0; i<state.grid.length; i++){
+        for(let j=0; j<state.grid[i].length; j++){
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
 function drawBox(container, row, col, letter=''){
     const box = document.createElement('div');
     box.className ='box';
@@ -20,9 +37,16 @@ function drawGrid(container) {
     container.appendChild(grid);
 }
 
+function registerKeyboardEvents() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+    }
+}
+
 function startup() {
     const game = document.getElementById('game');
     drawGrid(game);
+
 }
 
 startup();
