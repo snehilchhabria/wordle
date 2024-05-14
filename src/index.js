1e7d058d74534a0b948b35d4b9e734fbf42337d1
Snehil Chhabria <snehilchhabria143@gmail.com>
2024-05-14 23:25:42 +0530

    logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-alert('hello');
+function drawBox(container, row, col, letter=''){
+    const box = document.createElement('div');
+    box.className ='box';
+    box.id = `box${row}${col}`;
+    box.textContent = letter;
+
+    container.appendChild(box);
+    return box;
+}
+
+function drawGrid(container) {
+    const grid = document.createElement('div')
+    grid.className = 'grid';
+
+    for(let i = 0; i < 6; i++){
+        for(let j = 0; j < 5; j++){
+            drawBox(grid, i, j);
+        }
+    }
+    container.appendChild(grid);
+}
+
+function startup() {
+    const game = document.getElementById('game');
+    drawGrid(game);
+}
+
+startup();
